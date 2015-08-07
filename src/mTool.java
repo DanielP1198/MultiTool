65fe4147db32d1d9ee7ae5473f590819cfc97cbd
Daniel <danielperezz1198@gmail.com>
2015-08-07 03:01:32 -0400

    so far

--- src/mTool.java
+++ src/mTool.java
@@ -1,10 +1,14 @@
 import java.awt.BorderLayout;
 import java.awt.ComponentOrientation;
+import java.awt.Container;
 import java.awt.FlowLayout;
+import java.awt.Frame;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -15,10 +19,9 @@ public class mTool extends JFrame {
 	
 	public static String Tool; 
 	
-		
-	
  
 	public static void main(String args[]){
+		
 		//Window characteristics
 		mTool frame = new mTool();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -27,31 +30,114 @@ public class mTool extends JFrame {
 		frame.setTitle("MultiTool");
 		frame.setResizable(false);
 		
+		//THIS IS THE MENU PANEL
 		JPanel panel = new JPanel();
 		panel.setLayout(new GridBagLayout());
 		GridBagConstraints gbc = new GridBagConstraints();
 		gbc.insets = new Insets(10,10,10,10);
 		JButton tCalc = new JButton("Calculator");
-		JButton t2 = new JButton("Tool2");
+		tCalc.addActionListener(new Action());
+		JButton tEditor = new JButton("Text Editor");
+		tEditor.addActionListener(new Action());
+		JButton tGame = new JButton("Game");
+		tGame.addActionListener(new Action());
 		frame.add(panel,BorderLayout.NORTH);
 		panel.add(tCalc,gbc);
-		panel.add(t2,gbc);
+		panel.add(tEditor,gbc);
+		panel.add(tGame,gbc);
 		
-		if(Tool != null){initUi();}
+	
+		
+	
+		
+	}
+	// THIS ACTUALLY DOESNT ACTUALLY WORK
+	// HAVE TO USE CARD LAYOUT ^^^^
+	
+	/*
+	public static void initUi(){
+		
+		
+		JPanel calcPanel = new JPanel();
+		JPanel editPanel = new JPanel();
+		JPanel gamePanel = new JPanel();
+		calcPanel.setLayout(new GridBagLayout());
+		editPanel.setLayout(new GridBagLayout());
+		gamePanel.setLayout(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.insets = new Insets(15,15,15,15);
+		//Calc
+		JTextField num1 = new JTextField();
+		JTextField num2 = new JTextField();
+		JButton adding = new JButton("Add");
+		
+		//THESE ARE THE SEPERATE TOOL PANELS
+		
+		
+		
+		
+		/*if(Tool == "Calculator"){
+			frame.add(calcPanel,gbc);
+			calcPanel.add(num1,gbc);
+			calcPanel.add(num2,gbc);
+			calcPanel.add(adding,gbc);
+			System.out.println("Hello");
+		
+		}else{
+			frame.remove(calcPanel);
+			
+		}
+		
+		
+		
+		
+		if(Tool == "Text Editor"){
+			frame.add(editPanel,gbc);
+		}
+		else{
+			frame.remove(editPanel);
+		}
+		
+		
+		
+		if(Tool == "Game"){
+			frame.add(gamePanel,gbc);
+		}
+		else{
+			frame.remove(gamePanel);
+		}
+	
+	
+	
+		
+	}
+	*/
+	static class Action implements ActionListener{
+		
+	
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+		String name = e.getActionCommand();
+		
+		if(name.equals("Calculator")){
+			Tool = name;
+		}else if(name.equals("Text Editor")){
+			Tool = name;
+		}else if(name.equals("Game")){
+			Tool = name;
+		}
+		//initUi();
+		
+		}
 		
 	}
 	
-	public static void initUi(){
-		if(Tool == "Calculator"){
-			
-		}
-		else{
-			
-		}
-		
-		
-			
-	}
+	
+
+	
+	
+
 	
 	
 }
