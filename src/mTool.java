e6d62f4b05190fff470258d63bb14494d487acdd
Daniel <danielperezz1198@gmail.com>
2015-08-07 19:18:55 -0400

    now can change panels and buttons

--- src/mTool.java
+++ src/mTool.java
@@ -1,4 +1,5 @@
 import java.awt.BorderLayout;
+import java.awt.CardLayout;
 import java.awt.ComponentOrientation;
 import java.awt.Container;
 import java.awt.FlowLayout;
@@ -12,12 +13,15 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class mTool extends JFrame {
 	
 	public static String Tool; 
+	public static double answ;
 	
  
 	public static void main(String args[]){
@@ -35,27 +39,17 @@ public class mTool extends JFrame {
 		panel.setLayout(new GridBagLayout());
 		GridBagConstraints gbc = new GridBagConstraints();
 		gbc.insets = new Insets(10,10,10,10);
+		
 		JButton tCalc = new JButton("Calculator");
-		tCalc.addActionListener(new Action());
 		JButton tEditor = new JButton("Text Editor");
-		tEditor.addActionListener(new Action());
 		JButton tGame = new JButton("Game");
-		tGame.addActionListener(new Action());
 		frame.add(panel,BorderLayout.NORTH);
 		panel.add(tCalc,gbc);
 		panel.add(tEditor,gbc);
 		panel.add(tGame,gbc);
 		
-	
-		
-	
-		
-	}
-	// THIS ACTUALLY DOESNT ACTUALLY WORK
-	// HAVE TO USE CARD LAYOUT ^^^^
-	
-	/*
-	public static void initUi(){
+		JPanel panelCont = new JPanel();
+		CardLayout cl = new CardLayout();
 		
 		
 		JPanel calcPanel = new JPanel();
@@ -64,80 +58,76 @@ public class mTool extends JFrame {
 		calcPanel.setLayout(new GridBagLayout());
 		editPanel.setLayout(new GridBagLayout());
 		gamePanel.setLayout(new GridBagLayout());
-		GridBagConstraints gbc = new GridBagConstraints();
-		gbc.insets = new Insets(15,15,15,15);
-		//Calc
-		JTextField num1 = new JTextField();
-		JTextField num2 = new JTextField();
+		
+		panelCont.setLayout(cl);
+		
+		JTextField num1 = new JTextField(10);
+		JTextField num2 = new JTextField(10);
 		JButton adding = new JButton("Add");
+		JLabel answer = new JLabel();
 		
-		//THESE ARE THE SEPERATE TOOL PANELS
+		calcPanel.add(num1,gbc);
+		calcPanel.add(num2,gbc);
+		calcPanel.add(answer, gbc);
+		calcPanel.add(adding,gbc);
+		
+		panelCont.add(calcPanel, "Calculator");
+		panelCont.add(editPanel, "Editor");
+		panelCont.add(gamePanel, "Game");
+		cl.show(panelCont, "Calculator");
+		
+		//This is the panel events
+		
+		tCalc.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				cl.show(panelCont, "Calculator");
+					
+			}
+		});
+		tEditor.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				cl.show(panelCont, "Editor");
+					
+			}
+		});
+		tGame.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				cl.show(panelCont, "Game");
+					
+			}
+		});
+		frame.add(panelCont);
+		//Calc buttons
+		
+		//Adding
+		adding.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				double Num1 = 0;
+				double Num2 = 0;
+				try {
+					Num1 = Double.parseDouble(num1.getText());
+					Num2 = Double.parseDouble(num2.getText());
+				} catch (NumberFormatException error) {
+					error.printStackTrace();
+					JOptionPane.showMessageDialog(null, "Please enter a valid number");
+				}
+
+				if(Num1 != 0 || Num2 != 0){
+				answ = Num1+Num2;
+				answer.setText(""+(int)answ);
+				System.out.println(answ);
+				}
+			}
+		});
 		
 		
 		
-		
-		/*if(Tool == "Calculator"){
-			frame.add(calcPanel,gbc);
-			calcPanel.add(num1,gbc);
-			calcPanel.add(num2,gbc);
-			calcPanel.add(adding,gbc);
-			System.out.println("Hello");
-		
-		}else{
-			frame.remove(calcPanel);
-			
-		}
-		
-		
-		
-		
-		if(Tool == "Text Editor"){
-			frame.add(editPanel,gbc);
-		}
-		else{
-			frame.remove(editPanel);
-		}
-		
-		
-		
-		if(Tool == "Game"){
-			frame.add(gamePanel,gbc);
-		}
-		else{
-			frame.remove(gamePanel);
-		}
 	
-	
-	
-		
-	}
-	*/
-	static class Action implements ActionListener{
-		
-	
-		
-		@Override
-		public void actionPerformed(ActionEvent e) {
-		String name = e.getActionCommand();
-		
-		if(name.equals("Calculator")){
-			Tool = name;
-		}else if(name.equals("Text Editor")){
-			Tool = name;
-		}else if(name.equals("Game")){
-			Tool = name;
-		}
-		//initUi();
-		
-		}
 		
 	}
 	
 	
 
-	
-	
-
-	
 	
 }
